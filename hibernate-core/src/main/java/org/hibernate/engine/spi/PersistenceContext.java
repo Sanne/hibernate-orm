c07f14ffe0b2dfcd2796c0c3440d289863b31c47
Andrea Boriero <andrea@hibernate.org>
2019-07-11 11:40:12 +0100

    6 - SQM based on JPA type system

--- hibernate-core/src/main/java/org/hibernate/engine/spi/PersistenceContext.java
+++ hibernate-core/src/main/java/org/hibernate/engine/spi/PersistenceContext.java
@@ -407,7 +407,7 @@ public interface PersistenceContext {
 	CollectionEntry addInitializedCollection(
 			CollectionPersister persister,
 			PersistentCollection collection,
-			Object id);
+			Serializable id);
 
 	/**
 	 * Get the collection instance associated with the <tt>CollectionKey</tt>
