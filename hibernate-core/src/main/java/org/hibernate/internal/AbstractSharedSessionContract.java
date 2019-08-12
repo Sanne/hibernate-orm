8ea329492cfb13ce54680c165ebc6509964cca4c
Sanne Grinovero <sanne@hibernate.org>
2019-08-12 09:41:49 +0100

    Promote JDBCServices as hot service to be retrived from FastSessionService as well

--- hibernate-core/src/main/java/org/hibernate/internal/AbstractSharedSessionContract.java
+++ hibernate-core/src/main/java/org/hibernate/internal/AbstractSharedSessionContract.java
@@ -213,7 +213,7 @@ public abstract class AbstractSharedSessionContract implements SharedSessionCont
 		else {
 			this.isTransactionCoordinatorShared = false;
 			this.autoJoinTransactions = options.shouldAutoJoinTransactions();
-			this.jdbcCoordinator = new JdbcCoordinatorImpl( options.getConnection(), this );
+			this.jdbcCoordinator = new JdbcCoordinatorImpl( options.getConnection(), this, fastSessionServices.jdbcServices );
 			this.transactionCoordinator = fastSessionServices.transactionCoordinatorBuilder.buildTransactionCoordinator( jdbcCoordinator, this );
 		}
 	}
