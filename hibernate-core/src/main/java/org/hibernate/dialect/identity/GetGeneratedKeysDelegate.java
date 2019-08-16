bf9449a1b9ba5edcf20c90ecdbcac8236845e735
Sanne Grinovero <sanne@hibernate.org>
2019-08-16 16:15:07 +0100

    Statements are always released by the caller

--- hibernate-core/src/main/java/org/hibernate/dialect/identity/GetGeneratedKeysDelegate.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/identity/GetGeneratedKeysDelegate.java
@@ -55,20 +55,12 @@ public class GetGeneratedKeysDelegate
 	public Serializable executeAndExtract(PreparedStatement insert, SharedSessionContractImplementor session)
 			throws SQLException {
 		session.getJdbcCoordinator().getResultSetReturn().executeUpdate( insert );
-		ResultSet rs = null;
-		try {
-			rs = insert.getGeneratedKeys();
-			return IdentifierGeneratorHelper.getGeneratedIdentity(
-					rs,
-					persister.getRootTableKeyColumnNames()[0],
-					persister.getIdentifierType(),
-					session.getJdbcServices().getJdbcEnvironment().getDialect()
-			);
-		}
-		finally {
-			if ( rs != null ) {
-				session.getJdbcCoordinator().getLogicalConnection().getResourceRegistry().release( rs, insert );
-			}
-		}
+		ResultSet rs = insert.getGeneratedKeys();
+		return IdentifierGeneratorHelper.getGeneratedIdentity(
+				rs,
+				persister.getRootTableKeyColumnNames()[0],
+				persister.getIdentifierType(),
+				session.getJdbcServices().getJdbcEnvironment().getDialect()
+		);
 	}
 }
--- hibernate-core/src/main/java/org/hibernate/id/IdentityGenerator.java
+++ hibernate-core/src/main/java/org/hibernate/id/IdentityGenerator.java
@@ -85,17 +85,12 @@ public class IdentityGenerator extends AbstractPostInsertGenerator {
 		public Serializable executeAndExtract(PreparedStatement insert, SharedSessionContractImplementor session)
 				throws SQLException {
 			ResultSet rs = session.getJdbcCoordinator().getResultSetReturn().execute( insert );
-			try {
-				return IdentifierGeneratorHelper.getGeneratedIdentity(
-						rs,
-						persister.getRootTableKeyColumnNames()[0],
-						persister.getIdentifierType(),
-						session.getJdbcServices().getJdbcEnvironment().getDialect()
-				);
-			}
-			finally {
-				session.getJdbcCoordinator().getLogicalConnection().getResourceRegistry().release( rs, insert );
-			}
+			return IdentifierGeneratorHelper.getGeneratedIdentity(
+					rs,
+					persister.getRootTableKeyColumnNames()[0],
+					persister.getIdentifierType(),
+					session.getJdbcServices().getJdbcEnvironment().getDialect()
+			);
 		}
 
 		public Serializable determineGeneratedIdentifier(SharedSessionContractImplementor session, Object entity) {
