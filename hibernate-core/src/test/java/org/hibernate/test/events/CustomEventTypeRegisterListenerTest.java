4af938848f4ad4f9452f4aba61a319f69db7157e
Sanne Grinovero <sanne@hibernate.org>
2020-04-21 23:22:27 +0100

    Apply the scripts

--- hibernate-core/src/test/java/org/hibernate/test/events/CustomEventTypeRegisterListenerTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/events/CustomEventTypeRegisterListenerTest.java
@@ -7,8 +7,8 @@
 package org.hibernate.test.events;
 
 import java.util.concurrent.atomic.AtomicInteger;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 import org.hibernate.HibernateException;
 import org.hibernate.boot.Metadata;
